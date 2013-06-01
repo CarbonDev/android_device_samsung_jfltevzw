d5fb3a44bb1f5523de82d959b21e324581017a54
dg4prez <mrdgelinas@gmail.com>
2013-05-31 22:35:00 -0400

    Carbonification process complete.

---
+++ carbon.mk
@@ -0,0 +1,16 @@
+$(call inherit-product, device/samsung/jfltevzw/full_jfltevzw.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/carbon/config/common_cdma.mk)
+
+# Enhanced NFC
+#$(call inherit-product, vendor/carbon/config/nfc_enhanced.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.2.2/JDQ39/I545VRUAMDK:user/release-keys" PRIVATE_BUILD_DESC="jfltevzw-user 4.2.2 JDQ39 I545VRUAMDK release-keys"
+
+PRODUCT_NAME := carbon_jfltevzw
+PRODUCT_DEVICE := jfltevzw
+
