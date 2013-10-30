53a454b073d29816986c1995dca789ad93c8af7c
bigshotrob22 <rrascon22@gmail.com>
2013-10-30 15:00:24 -0700

    Remove common_cdma.mk import
    Change-Id: I2a3f9386cbe6b33b5bb2e858bf3008e2228b0d61

--- carbon.mk
+++ carbon.mk
@@ -1,7 +1,5 @@
 $(call inherit-product, device/samsung/jfltevzw/full_jfltevzw.mk)
 
-$(call inherit-product, vendor/carbon/config/common_cdma.mk)
-
 $(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.2.2/JDQ39/I545VRUAMDK:user/release-keys" PRIVATE_BUILD_DESC="jfltevzw-user 4.2.2 JDQ39 I545VRUAMDK release-keys"
